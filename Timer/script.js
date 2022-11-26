91fe9a5657e7e4c492f843dd72345e878a67ff4d
Jon Marc Jackson <113649613+JMAJack@users.noreply.github.com>
2022-11-25 21:26:24 -0500

    timer ig

--- Timer/script.js
+++ Timer/script.js
@@ -3,13 +3,15 @@ let time = startingSeconds * 60;
 
 const countdownEL = document.getElementById('countdown');
 
-setInterval(updatetimer,1000)
+setInterval(updatetimer, 1000)
 
 function updatetimer()
 {
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
 
-    countdownEL.innerHTML = '${minutes}: ${seconds} ';
+    seconds = seconds < 10 ? '0'  + seconds : seconds;
+
+    countdownEL.innerHTML = "${minutes}: ${seconds}";
     time--;
 }
